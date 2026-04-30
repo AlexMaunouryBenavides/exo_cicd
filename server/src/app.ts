b504560d31980eccc3a2320b372560120a2ebfbd
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-04-30 15:11:48 +0200

    feat: instaling jest with bug

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,3 @@
-import "reflect-metadata";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -6,23 +5,23 @@ import express from "express";
 import userrouter from "./modules/user/presentation/user.route.ts";
 import { AppDataSource } from "./config/data-source.ts";
 dotenv.config();
-try { await AppDataSource.initialize()
-  console.log("data source has been initialize")
-  
-} catch (error) {console.log("error occured", error)
-  
+try {
+	await AppDataSource.initialize();
+	console.log("data source has been initialize");
+} catch (error) {
+	console.log("error occured", error);
 }
 const app = express();
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-app.use("/", userrouter)
+app.use("/", userrouter);
 const port = 3000;
 
 app.get("/", (_, res) => {
-  res.send("API Lancé");
+	res.send("API Lancé");
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
