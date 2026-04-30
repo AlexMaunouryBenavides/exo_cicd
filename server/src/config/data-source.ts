72c7dc304df8d9e79c2252cd56dd9093b9d0d90d
medinazen <anisboudjilal16@gmail.com>
2026-04-30 16:51:58 +0200

    biome config

--- server/src/config/data-source.ts
+++ server/src/config/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST || "error",
   port: parseInt(process.env.DB_PORT || "3306", 10),
   username: process.env.DB_USER || "error",
-  password: process.env.DB_PASSWORD || "error", 
+  password: process.env.DB_PASSWORD || "error",
   database: process.env.DB_NAME || "error",
   synchronize: true,
   logging: true,
