2ac967f64659c9f336c1ed1cc2c3309d6ba1c701
medinazen <anisboudjilal16@gmail.com>
2026-04-30 16:25:22 +0200

    biome config

--- server/src/config/data-source.ts
+++ server/src/config/data-source.ts
@@ -7,11 +7,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST!,
+  host: process.env.DB_HOST || "error",
   port: parseInt(process.env.DB_PORT || "3306", 10),
-  username: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
+  username: process.env.DB_USER || "error",
+  password: process.env.DB_PASSWORD || "error", 
+  database: process.env.DB_NAME || "error",
   synchronize: true,
   logging: true,
   entities: [User],
