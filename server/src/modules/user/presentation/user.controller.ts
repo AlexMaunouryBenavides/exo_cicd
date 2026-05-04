95874ed00b8a75cb2d0b36e2bf3457a51383abbe
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-05-04 11:55:55 +0200

    feat: add ci typescheck

--- server/src/modules/user/presentation/user.controller.ts
+++ server/src/modules/user/presentation/user.controller.ts
@@ -6,97 +6,97 @@ import { UserRepository } from "../domain/user.repository.js";
 const repo = new UserRepository();
 
 export class UserController {
-	async browse(_: Request, res: Response, next: NextFunction) {
-		try {
-			const users = await repo.read();
-			res.json(users).status(200);
-		} catch (error) {
-			next(error);
-		}
-	}
-	async read(req: Request, res: Response, next: NextFunction) {
-		try {
-			const { email } = req.params;
+  async browse(_: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await repo.read();
+      res.json(users).status(200);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email } = req.params;
 
-			if (!email || Array.isArray(email)) {
-				res.status(400).json({ message: "Invalid email parameter" });
-				return;
-			}
-			const user = await repo.readOne(email);
-			if (!user) {
-				res.status(404).json({ message: "User not found" });
-				return;
-			}
-			res.json(user).status(200);
-		} catch (error) {
-			next(error);
-		}
-	}
-	async add(req: Request, res: Response, next: NextFunction) {
-		try {
-			const { username, email, password } = req.body;
-			if (!username || !email || !password) {
-				res.status(400).json({ message: "Missing required fields" });
-				return;
-			}
-			const hashpassord = await hashPassord(password);
-			const user = repo.create({ username, email, password: hashpassord });
-			const savedUser = repo.save(user);
+      if (!email || Array.isArray(email)) {
+        res.status(400).json({ message: "Invalid email parameter" });
+        return;
+      }
+      const user = await repo.readOne(email);
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+      res.json(user).status(200);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async add(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { username, email, password } = req.body;
+      if (!username || !email || !password) {
+        res.status(400).json({ message: "Missing required fields" });
+        return;
+      }
+      const hashpassord = await hashPassord(password);
+      const user = repo.create({ username, email, password: hashpassord });
+      const savedUser = repo.save(user);
 
-			res.status(201).json(savedUser);
-		} catch (error) {
-			next(error);
-		}
-	}
+      res.status(201).json(savedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
 
-	async edit(
-		req: Request<{ email: string }>,
-		res: Response,
-		next: NextFunction,
-	) {
-		try {
-			const email = req.params.email;
-			if (!email || Array.isArray(email)) {
-				res.status(400).json({ message: "Invalid email parameter" });
-				return;
-			}
-			const user = await repo.readOne(email);
-			if (!user) {
-				res.status(404).json({ message: "User not found" });
-				return;
-			}
-			const { username, password } = req.body;
-			const updates: Partial<User> = {};
-			if (username) updates.username = username;
-			if (password) updates.password = password;
+  async edit(
+    req: Request<{ email: string }>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const email = req.params.email;
+      if (!email || Array.isArray(email)) {
+        res.status(400).json({ message: "Invalid email parameter" });
+        return;
+      }
+      const user = await repo.readOne(email);
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+      const { username, password } = req.body;
+      const updates: Partial<User> = {};
+      if (username) updates.username = username;
+      if (password) updates.password = password;
 
-			repo.merge(user, updates);
-			const updatedUser = await repo.update(user);
-			res.json(updatedUser).status(200);
-		} catch (error) {
-			next(error);
-		}
-	}
-	async destroy(
-		req: Request<{ email: string }>,
-		res: Response,
-		next: NextFunction,
-	) {
-		try {
-			const email = req.params.email;
-			if (!email || Array.isArray(email)) {
-				res.status(400).json({ message: "Invalid email parameter" });
-				return;
-			}
-			const user = await repo.readOne(email);
-			if (!user) {
-				res.status(404).json({ message: "User not found" });
-				return;
-			}
-			await repo.delete(user);
-			res.sendStatus(204);
-		} catch (error) {
-			next(error);
-		}
-	}
+      repo.merge(user, updates);
+      const updatedUser = await repo.update(user);
+      res.json(updatedUser).status(200);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async destroy(
+    req: Request<{ email: string }>,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const email = req.params.email;
+      if (!email || Array.isArray(email)) {
+        res.status(400).json({ message: "Invalid email parameter" });
+        return;
+      }
+      const user = await repo.readOne(email);
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+      await repo.delete(user);
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
--- server/src/tests/unit/user.test.ts
+++ server/src/tests/unit/user.test.ts
@@ -4,24 +4,24 @@ import { UserController } from "../../modules/user/presentation/user.controller.
 
 jest.mock("../../modules/user/domain/user.repository.ts");
 describe("UserController", () => {
-	let req: Partial<Request>;
-	let res: Partial<Response>;
-	let next: NextFunction;
-	let controller: UserController;
-	beforeEach(() => {
-		req = {};
-		res = {
-			json: jest.fn().mockReturnThis(),
-			status: jest.fn().mockReturnThis(),
-		};
-		next = jest.fn();
-		controller = new UserController();
-	});
-	it("should return all users", async () => {
-		const users = [{ id: 1, username: "test", email: "test@example.com" }];
-		(UserRepository.prototype.read as jest.Mock).mockResolvedValue(users);
-		await controller.browse(req as Request, res as Response, next);
-		expect(res.json).toHaveBeenCalledWith(users);
-		expect(res.status).toHaveBeenCalledWith(200);
-	});
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+  let controller: UserController;
+  beforeEach(() => {
+    req = {};
+    res = {
+      json: jest.fn().mockReturnThis(),
+      status: jest.fn().mockReturnThis(),
+    };
+    next = jest.fn();
+    controller = new UserController();
+  });
+  it("should return all users", async () => {
+    const users = [{ id: 1, username: "test", email: "test@example.com" }];
+    (UserRepository.prototype.read as jest.Mock).mockResolvedValue(users);
+    await controller.browse(req as Request, res as Response, next);
+    expect(res.json).toHaveBeenCalledWith(users);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
 });
